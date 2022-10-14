58f29f20300359bc0b9047d2442512f2ea6a7abe
CJB <joonbin@naver.com>
2022-10-14 13:30:38 +0900

    [CJB] login sample

---
+++ login_dice-master/android/app/src/main/java/com/example/logindice/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.logindice;
+
+//import android.support.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
---
+++ login_dice-master/lib/dice.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Dice extends StatelessWidget {
+  const Dice({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
