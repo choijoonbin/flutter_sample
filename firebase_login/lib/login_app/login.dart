2c3e39aa77f9b53e35429ff5ab66f8230fbc61b0
CJB <joonbin@naver.com>
2022-10-18 10:40:38 +0900

    [CJB] firebase

--- firebase_login/lib/login_app/login.dart
+++ firebase_login/lib/login_app/login.dart
@@ -1,22 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Firebase Login app',
-      theme: ThemeData(primarySwatch: Colors.grey),
-      home: LogIn(),
-    );
-  }
-}
-
-class LogIn extends StatelessWidget {
-  const LogIn({Key? key}) : super(key: key);
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
